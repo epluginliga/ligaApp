fdc81e71bc2eca91096f8361d07256ac43824d77
Jean Marcos <jean.silva@eplugin.app.br>
2024-07-22 10:17:11 -0300

    feat: add nome usuario banco de dados app

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -10,13 +10,19 @@ import { login, LoginProps, UserProps } from '../services/auth';
 import { useMutation, UseMutationResult } from '@tanstack/react-query';
 import { MMKV } from 'react-native-mmkv'
 import api from '../services';
+
+export type UserAuthDados = {
+   id: string;
+   nome: string;
+}
+
 interface AuthContextProps {
    handleSignIn: UseMutationResult<UserProps, Error, LoginProps, unknown>
    token: string;
    logado: boolean;
    loading: boolean;
    signOut: () => void;
-   user_id: string;
+   user?: UserAuthDados;
 }
 interface AuthProviderProps {
    children: React.ReactNode;
@@ -25,43 +31,52 @@ interface AuthProviderProps {
 export const usuarioStorage = new MMKV()
 const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
 
+
+
 function AuthProvider({ children }: AuthProviderProps): React.ReactElement {
    const [token, setToken] = useState('');
    const [loading, setLoading] = useState(true);
-   const [user, setUser] = useState('');
+   const [user, setUser] = useState({} as UserAuthDados);
 
    const handleSignIn = useMutation({
       mutationKey: ['handleLogin'],
       mutationFn: (data: LoginProps) => login(data),
       onSuccess(data) {
+         console.log(JSON.stringify(data, null, 1));
+
          if (!data) {
             throw new Error("Dados inválidos!");
          }
 
+         const dadosUsuario = {
+            id: data.user_id,
+            nome: data.name
+         };
+
          usuarioStorage.set('token', data.api_token);
-         usuarioStorage.set('user_id', data.user_id);
+         usuarioStorage.set('user_id', JSON.stringify(dadosUsuario));
 
          api.defaults.headers.Authorization = data.api_token;
          setToken(data.api_token);
-         setUser(data.user_id)
+         setUser(dadosUsuario)
       },
    });
 
    const signOut = useCallback(() => {
       usuarioStorage.clearAll();
       setToken('');
-      setUser('');
+      setUser({} as UserAuthDados);
       delete api.defaults.headers.Authorization;
    }, []);
 
    useEffect(() => {
       let token = usuarioStorage.getString('token');
-      const user_id = usuarioStorage.getString('user_id');
+      let user = usuarioStorage.getString('user_id');
 
-      if (token && user_id) {
+      if (token && user) {
          api.defaults.headers.Authorization = token;
          setToken(token);
-         setUser(user_id);
+         setUser(JSON.parse(user));
       }
 
       setLoading(false)
@@ -76,7 +91,7 @@ function AuthProvider({ children }: AuthProviderProps): React.ReactElement {
             logado: !!token,
             loading: handleSignIn.isPending || loading,
             signOut,
-            user_id: user
+            user
          }}>
          {children}
       </AuthContext.Provider>
