68bbac45365121b11f4e354ef3b4bd496f3d9267
Jean Marcos <jean.silva@eplugin.app.br>
2025-04-14 11:00:13 -0300

    feat: atualizar configurações de ambiente e ESLint, remover Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+     quotes: ['error', 'single'],
+     'eol-last': true,
+     semi: ['error', 'always'],
+     indent: ['error', 3], // Configura a indentação como 3 espaços.
+  },
 };
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+   arrowParens: 'avoid',
+   bracketSameLine: false,
+   singleQuote: true,
+   trailingComma: 'all',
+   bracketSpacing: true,
+   tabWidth: 3,
+};
