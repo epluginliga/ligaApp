350a385135cfba24a10bc3580dda6e7bfc024919
Jean Marcos <jean.silva@eplugin.app.br>
2025-04-14 10:59:57 -0300

    feat: adicionar componente Slide e integrar na tela de Eventos

---
+++ src/components/Slide/index.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef } from 'react';
+import { FlatList, FlatListProps, StyleSheet, View } from 'react-native';
+
+type SlideProps<T> = FlatListProps<T> & {
+   data: T[];
+};
+export function Slide<T>({ data, ...rest }: SlideProps<T>) {
+   const flatListRef = useRef<FlatList<T>>(null);
+
+   useEffect(() => {
+      if (data?.length) {
+         let index = 0;
+         const interval = setInterval(() => {
+            if (flatListRef.current) {
+               index = (index + 1) % data?.length;
+               flatListRef.current.scrollToIndex({ animated: true, index });
+            }
+         }, 3000); // muda a cada 3 segundos
+
+         return () => clearInterval(interval);
+      }
+   }, [data]);
+
+   if (!data) return;
+
+   return (
+      <FlatList
+         contentContainerStyle={{ paddingVertical: 10 }}
+         ref={flatListRef}
+         data={data}
+         horizontal
+         pagingEnabled
+         ItemSeparatorComponent={() => <View style={styles.spacer} />}
+         // renderItem={ItemBannerTurma}
+         // keyExtractor={item => item.id.toString()}
+         showsHorizontalScrollIndicator={false}
+         {...rest}
+      />
+   );
+}
+
+const styles = StyleSheet.create({
+   spacer: {
+      height: 10,
+   },
+});
--- src/screens/Eventos/index.tsx
+++ src/screens/Eventos/index.tsx
@@ -1,76 +1,54 @@
-import React, { useCallback, useEffect } from "react";
-import { FlatList, RefreshControl } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { useQuery } from "@tanstack/react-query";
+import React, { useCallback, useEffect } from 'react';
+import { FlatList, Pressable, RefreshControl } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useQuery } from '@tanstack/react-query';
 
-import Text from "../../components/Text";
-import { Card } from "../../components/Card";
-import VStack from "../../components/Views/Vstack";
-import HStack from "../../components/Views/Hstack";
-import { Icon } from "../../icons";
-import { fetchEventos } from "../../services/eventos";
-import { ListEmptyComponent } from "../../components/ListEmptyComponent";
-import { Layout } from "../../components/Views/Layout";
-import { dataApp } from "../../utils/utils";
-import { Imagem } from "../../components/Imagem";
-import { Button } from "../../components/Button";
-import { useMMKVString } from "react-native-mmkv";
-import { useAuth, usuarioStorage } from "../../hooks/auth";
-import { EventosPayload } from "../../services/@eventos";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { useTheme } from "@shopify/restyle";
-import { Theme } from "../../theme/default";
+import Text from '../../components/Text';
+import { Card } from '../../components/Card';
+import VStack from '../../components/Views/Vstack';
+import HStack from '../../components/Views/Hstack';
+import { Icon } from '../../icons';
+import { fetchEventos } from '../../services/eventos';
+import { ListEmptyComponent } from '../../components/ListEmptyComponent';
+import { Layout } from '../../components/Views/Layout';
+import { dataApp } from '../../utils/utils';
+import { Imagem } from '../../components/Imagem';
+import { Button } from '../../components/Button';
+import { useMMKVString } from 'react-native-mmkv';
+import { useAuth, usuarioStorage } from '../../hooks/auth';
+import { EventosPayload } from '../../services/@eventos';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useTheme } from '@shopify/restyle';
+import { Theme } from '../../theme/default';
+import { Slide } from '../../components/Slide';
 
 export type ItemData = {
    item: EventosPayload;
+};
+
+function Item({ item }: { item: EventosPayload }) {
+   return (
+      <Pressable onPress={() => console.log('')}>
+         <VStack overflow="hidden" px="xs">
+            <Imagem source={{ uri: item.path_imagem }} />
+         </VStack>
+      </Pressable>
+   );
 }
 
 type DestaqueProps = {
-   evento?: EventosPayload;
-}
+   evento?: EventosPayload[];
+};
 function Destaque({ evento }: DestaqueProps) {
-   const navigate = useNavigation();
 
    if (!evento) return null;
 
-   const dataEvento = dataApp(evento.data_evento);
-
-   return (
-      <VStack position='relative' alignItems="center" overflow='hidden' borderRadius={10}>
-         <Imagem style={{ position: "relative" }} source={{ uri: evento.path_imagem }} >
-            <VStack zIndex={999} justifyContent="space-between" flex={1}>
-               <VStack gap="xs">
-                  <Text color="white">{dataEvento.diaSemana()} - <Text color='white' fontWeight="900">
-                     {dataEvento.diaMes()}</Text> de {dataEvento.nomeMes()}
-                  </Text>
-                  <Text fontSize={26} fontWeight="900" color="white">
-                     {evento.nome}
-                  </Text>
-                  <Text fontSize={16} color="white">
-                     <Text fontWeight="900" color="white">
-                        {evento.cidade} - {evento.estado}
-                     </Text>
-                     | {evento.nome_local}
-                  </Text>
-               </VStack>
-
-               <Button
-                  variant="link"
-                  onPress={() => navigate.navigate("EventosDetalhe", {
-                     id: evento.id,
-                  })}
-               >
-                  @Ver detalhes do evento
-               </Button>
-            </VStack>
-         </Imagem>
-      </VStack>
-   )
+   return <Slide data={evento} renderItem={Item} />;
 }
 
 export function Eventos() {
    const navigate = useNavigation();
-   const [route] = useMMKVString('route')
+   const [route] = useMMKVString('route');
    const { logado } = useAuth();
    const insets = useSafeAreaInsets();
    const { colors } = useTheme<Theme>();
@@ -86,48 +64,54 @@ export function Eventos() {
          <Card.Root
             marginHorizontal="sm"
             pr="xs"
-            onPress={() => navigate.navigate("EventosDetalhe", { id: item.id })}>
-
+            onPress={() => navigate.navigate('EventosDetalhe', { id: item.id })}
+         >
             <Card.Image
                flex={1}
                height={88}
-               source={{ uri: item.path_imagem }} />
+               source={{ uri: item.path_imagem }}
+            />
 
-            <VStack
-               flex={2}
-               justifyContent="space-evenly"
-               pb="sm">
-
+            <VStack flex={2} justifyContent="space-evenly" pb="sm">
                <Card.Title marginVertical="sm">{item.nome}</Card.Title>
 
                <HStack justifyContent="space-around" alignItems="center">
-
                   <VStack flex={1}>
                      <Card.SubTitle>
                         {item.nome_local} {'\n'}
                         <Card.Span>
                            <Icon.Pin size={16} />
-                           {item.cidade} | {item.estado} - {diaEvento.hora() || 'hora não definida'}
+                           {item.cidade} | {item.estado} -{' '}
+                           {diaEvento.hora() || 'hora não definida'}
                         </Card.Span>
                      </Card.SubTitle>
                   </VStack>
 
                   <VStack>
                      <Card.Widget>
-                        <Text textAlign="center" color="white" fontWeight="700" fontSize={22}>
+                        <Text
+                           textAlign="center"
+                           color="white"
+                           fontWeight="700"
+                           fontSize={22}
+                        >
                            {diaEvento.diaMes()}
                         </Text>
-                        <Text color="white" textTransform="uppercase" fontWeight="500" fontSize={14} style={{ marginTop: -8 }}>
+                        <Text
+                           color="white"
+                           textTransform="uppercase"
+                           fontWeight="500"
+                           fontSize={14}
+                           style={{ marginTop: -8 }}
+                        >
                            {diaEvento.nomeMes()}
                         </Text>
                      </Card.Widget>
                   </VStack>
-
-
                </HStack>
             </VStack>
          </Card.Root>
-      )
+      );
    }, []);
 
    useEffect(() => {
@@ -139,14 +123,13 @@ export function Eventos() {
       }, 500);
 
       return () => clearTimeout(time);
-
    }, [route, logado]);
 
    if (isLoading) {
       return null;
    }
 
-   const destaque = data?.data?.find(evento => !!evento.destaque);
+   const destaque = data?.data?.filter(evento => !!evento.destaque);
 
    return (
       <FlatList
@@ -162,9 +145,8 @@ export function Eventos() {
             paddingTop: insets.top,
             paddingBottom: insets.bottom,
          }}
-         ListHeaderComponent={(
+         ListHeaderComponent={
             <VStack gap="md" justifyContent="space-evenly" mb="md">
-
                <Destaque evento={destaque} />
 
                <VStack
@@ -172,15 +154,21 @@ export function Eventos() {
                   marginVertical="md"
                   pt="md"
                   marginHorizontal="sm"
-                  borderTopWidth={1}>
-                  <Text>Se <Text variant="header">LIGA</Text> no que está acontecendo</Text>
+                  borderTopWidth={1}
+               >
+                  <Text>
+                     Se <Text variant="header">LIGA</Text> no que está
+                     acontecendo
+                  </Text>
                </VStack>
             </VStack>
-         )}
-         ListEmptyComponent={<ListEmptyComponent title="Nenhum evento disponível" />}
+         }
+         ListEmptyComponent={
+            <ListEmptyComponent title="Nenhum evento disponível" />
+         }
          ItemSeparatorComponent={() => <VStack height={20} />}
          data={data?.data}
-         keyExtractor={(item) => item.id}
+         keyExtractor={item => item.id}
          renderItem={renderItem}
          ListFooterComponent={<VStack height={20} />}
          showsVerticalScrollIndicator={false}
