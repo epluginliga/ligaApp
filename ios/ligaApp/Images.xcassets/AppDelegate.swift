224ca24612666182bb511c8bf942fdd2c672bfcd
jeanmarcos552 <jean.silva552@gmail.com>
2025-11-02 04:54:54 -0300

    chore: update react-native and related packages to version 0.77.0

--- ios/ligaApp/Images.xcassets/AppDelegate.swift
+++ ios/ligaApp/Images.xcassets/AppDelegate.swift
@@ -2,29 +2,36 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import FirebaseCore
 
 @main
 class AppDelegate: RCTAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    self.moduleName = "ligaApp"
-    self.dependencyProvider = RCTAppDependencyProvider()
+   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      FirebaseApp.configure()
+      self.moduleName = "ligaApp"
+      self.dependencyProvider = RCTAppDependencyProvider()
 
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
-    self.initialProps = [:]
+      // You can add your custom initial props in the dictionary below.
+      // They will be passed down to the ViewController used by React Native.
+      self.initialProps = [:]
 
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+   }
 
-  override func sourceURL(for bridge: RCTBridge) -> URL? {
-    self.bundleURL()
-  }
+   override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+      // Passa o link para o módulo Linking do React Native
+      return RCTLinkingManager.application(app, open: url, options: options)
+   }
 
-  override func bundleURL() -> URL? {
-#if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-#else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-#endif
-  }
+   override func sourceURL(for bridge: RCTBridge) -> URL? {
+      self.bundleURL()
+   }
+
+   override func bundleURL() -> URL? {
+      #if DEBUG
+         RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+      #else
+         Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+      #endif
+   }
 }
